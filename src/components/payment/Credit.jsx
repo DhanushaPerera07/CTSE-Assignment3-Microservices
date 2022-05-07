b149ba9839e5887c6970bb47b8b68f4d6e3fd6b2
R.A.D.B.S.PERERA_IT18231656 <it18231656@my.sliit.lk>
2022-05-07 10:53:42 +0530

    Add License file header and format the code.

--- src/components/payment/Credit.jsx
+++ src/components/payment/Credit.jsx
@@ -1,6 +1,29 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 Code4 v2 Technologies.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import React from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import {Button, Form} from 'react-bootstrap';
 import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {cartList} from '../product/ProductListItem';
@@ -24,9 +47,9 @@ export default class Credit extends React.Component {
         const {name, value} = event.target;
         this.setState({[name]: value});
     }
-    
+
     confirmPayment = () => {
-        const { address, email } = this.state.getData;
+        const {address, email} = this.state.getData;
         console.log(address, email);
 
         console.log(this.state.amount);
@@ -66,43 +89,42 @@ export default class Credit extends React.Component {
     render() {
         const notify = () =>
 
-        toast.success('Item purchased succesfully. Please check your mail inbox', {
-            position: 'top-right',
-            autoClose: 2000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-        });
+            toast.success('Item purchased succesfully. Please check your mail inbox', {
+                position: 'top-right',
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
 
-    
 
         return (
             <div className="container-sm">
-                <br />
+                <br/>
                 <h1>Payment Details</h1>
 
-                <br />
-                <br />
+                <br/>
+                <br/>
 
                 <Form>
 
                     <Form.Group controlId="formBasicCardHOlderName">
                         <Form.Label>Card holder's name</Form.Label>
                         <Form.Control name="cardHolderName"
-                            type="text"
-                            placeholder="Card holder's name"
-                            onChange={(event) => this.onChange(event)}
+                                      type="text"
+                                      placeholder="Card holder's name"
+                                      onChange={(event) => this.onChange(event)}
                         />
                     </Form.Group>
 
                     <Form.Group controlId="formBasicCardNumber">
                         <Form.Label>Card Number</Form.Label>
                         <Form.Control name="cardNumber"
-                            type="text"
-                            placeholder="Card Number"
-                            onChange={(event) => this.onChange(event)}
+                                      type="text"
+                                      placeholder="Card Number"
+                                      onChange={(event) => this.onChange(event)}
                         />
                     </Form.Group>
 
@@ -110,9 +132,9 @@ export default class Credit extends React.Component {
                     <Form.Group controlId="formBasicCVC">
                         <Form.Label>CVC Number</Form.Label>
                         <Form.Control name="cvcNumber"
-                            type="text"
-                            placeholder="CVC Number"
-                            onChange={(event) => this.onChange(event)}
+                                      type="text"
+                                      placeholder="CVC Number"
+                                      onChange={(event) => this.onChange(event)}
                         />
                     </Form.Group>
 
@@ -120,35 +142,34 @@ export default class Credit extends React.Component {
                     <Form.Group controlId="formBasicED">
                         <Form.Label>Expire Date</Form.Label>
                         <Form.Control name="expireyDate"
-                            type="text"
-                            placeholder="Expire Number"
-                            onChange={(event) => this.onChange(event)}
+                                      type="text"
+                                      placeholder="Expire Number"
+                                      onChange={(event) => this.onChange(event)}
                         />
                     </Form.Group>
-
 
 
                     <Form.Group controlId="formBasicAmount">
                         <Form.Label>Amount</Form.Label>
                         <Form.Control name="amount"
-                            type="text"
-                            placeholder="Amount"
-                            onChange={(event) => this.onChange(event)}
+                                      type="text"
+                                      placeholder="Amount"
+                                      onChange={(event) => this.onChange(event)}
                         />
                     </Form.Group>
 
                     <hr></hr>
-                    <br />
-                    <Button  onClick={() => {
-                                            this.confirmPayment();
-                                            notify();
-                                        }}
-                                        variant="primary"> Confirm
+                    <br/>
+                    <Button onClick={() => {
+                        this.confirmPayment();
+                        notify();
+                    }}
+                            variant="primary"> Confirm
                         {/* // TODO: if registration successful, then redirect to root URL (/product). */}
                         {/* <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>Confirm</Link> */}
                     </Button>
                 </Form>
-                <br />
+                <br/>
             </div>
         );
     }
--- src/components/product/ProductsHolder.jsx
+++ src/components/product/ProductsHolder.jsx
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 Code4 v2 Technologies.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import React from 'react';
 import Products from './Products';
 import {Redirect, Route, Switch} from 'react-router-dom';
@@ -7,43 +31,43 @@ import sha256 from 'crypto-js/sha256';
 
 export default class ProductsHolder extends React.Component {
 
-  static contextType = ProductContext;
+    static contextType = ProductContext;
 
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  /** method to add a new product. */
-  addProduct(product) {
-    return this.context.addProduct(product);
-  }
+    /** method to add a new product. */
+    addProduct(product) {
+        return this.context.addProduct(product);
+    }
 
-  /** method to update the product. */
-  updateProduct(product) {
-    return this.context.updateProduct(product);
-  }
+    /** method to update the product. */
+    updateProduct(product) {
+        return this.context.updateProduct(product);
+    }
 
-  render() {
-    // console.log('product holder works', this.context);
-    const isAdmin = (atob(sessionStorage.getItem(sha256(process.env.AUTHENTICATED_USER_TYPE))) === 'ADMIN');
-    return (
-        <div>
-          <Switch>
-            <Route exact path="/products">
-              {/* products component. */}
-              <Products products={(this.context?.products) ? this.context?.products : []}/>
-            </Route>
-            {/* Protected routes - if User Type == 'ADMIN' then only client can navigate to these routes. */}
-            <Route exact path="/products/add"
-                   render={(props) => ((isAdmin) ?
-                       <AddEditProduct {...props} saveOrUpdate={this.addProduct.bind(this)}/> :
-                       <Redirect to="/"/>)}/>
-            <Route exact path="/products/:productID/edit"
-                   render={(props) => ((isAdmin) ?
-                       <AddEditProduct {...props} saveOrUpdate={this.updateProduct.bind(this)}/> :
-                       <Redirect to="/"/>)}/>
-          </Switch>
-        </div>
-    );
-  }
+    render() {
+        // console.log('product holder works', this.context);
+        const isAdmin = (atob(sessionStorage.getItem(sha256(process.env.AUTHENTICATED_USER_TYPE))) === 'ADMIN');
+        return (
+            <div>
+                <Switch>
+                    <Route exact path="/products">
+                        {/* products component. */}
+                        <Products products={(this.context?.products) ? this.context?.products : []}/>
+                    </Route>
+                    {/* Protected routes - if User Type == 'ADMIN' then only client can navigate to these routes. */}
+                    <Route exact path="/products/add"
+                           render={(props) => ((isAdmin) ?
+                               <AddEditProduct {...props} saveOrUpdate={this.addProduct.bind(this)}/> :
+                               <Redirect to="/"/>)}/>
+                    <Route exact path="/products/:productID/edit"
+                           render={(props) => ((isAdmin) ?
+                               <AddEditProduct {...props} saveOrUpdate={this.updateProduct.bind(this)}/> :
+                               <Redirect to="/"/>)}/>
+                </Switch>
+            </div>
+        );
+    }
 }
